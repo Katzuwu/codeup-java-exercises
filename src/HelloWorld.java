bed671276288e4fc3ae70f95edaa90eb57ce579d
Ian Arellano <ian.g.arellano01@gmail.com>
2021-09-03 10:47:49 -0500

    completed HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,32 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello");
-        System.out.print(" World!");
+//        System.out.print("Hello");
+//        System.out.print(" World!");
+        int myFavoriteNumber = 69;
+        System.out.println(myFavoriteNumber);
+        String myString = "Did you know that the critically acclaimed MMORPG Final Fantasy XIV has a free trial, and includes the entirety of A Realm Reborn AND the award-winning Heavensward expansion up to level 60 with no restrictions on playtime? Sign up, and enjoy Eorzea today!";
+        System.out.println(myString);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        int x = 4;
+//        x += 5;
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -=x;
+        System.out.println(y);
+
+        System.out.println(Integer.MAX_VALUE);
+        int beegInteger = 2147483647;
+        System.out.println(++beegInteger);
     }
 }
