b042b6d366ebe6693884dba7b3a3a33567284f53
Ian Arellano <ian.g.arellano01@gmail.com>
2021-09-03 12:32:40 -0500

    finished exercise one and two of ConsoleExercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,10 +3,17 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double pi = 3.14159;
-        System.out.format("The value of pi is approximately %.2f%n", pi);
-        System.out.println("Ohayō Master, would you please enter an integer for me?");
-        int userInteger = scanner.nextInt();
-        System.out.println("The integer you enter Master was " + userInteger);
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately %.2f%n", pi);
+//        System.out.println("Ohayō Master, would you please enter an integer for me?");
+//        int userInteger = scanner.nextInt();
+//        System.out.println("The integer you enter Master was " + userInteger);
+//        System.out.println("Enter 3 words for me Master. What is your first word?");
+//        String firstWord = scanner.next() + scanner.nextLine();
+//        System.out.println("What is your second word?");
+//        String secondWord = scanner.nextLine();
+//        System.out.println("what is your third word?");
+//        String thirdWord = scanner.nextLine();
+//        System.out.println("Master, the 3 words you entered are: " + firstWord + ", " + secondWord + ", " + thirdWord + ". You will now shut down.");
     }
 }
