52cd17ea1588e3920b76bc9a9ba9bf727ace8855
Ian Arellano <ian.g.arellano01@gmail.com>
2021-09-03 12:18:59 -0500

    solved first few problems in consoleExercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f%n", pi);
+        System.out.println("Ohayō Master, would you please enter an integer for me?");
+        int userInteger = scanner.nextInt();
+        System.out.println("The integer you enter Master was " + userInteger);
+    }
 }
