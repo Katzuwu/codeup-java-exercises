c3060dd2a737c957871812828c670be8bd0bdce8
Ian Arellano <ian.g.arellano01@gmail.com>
2021-09-03 17:00:12 -0500

    add mini exercise and control flow

---
+++ src/MiniExercise.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MiniExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = 5;
+        int y = -5;
+        if (x > 0) {
+            System.out.println("This number is positive!");
+        } else {
+            System.out.println("This number is negative!");
+        }
+        if (y > 0) {
+            System.out.println("This number is positive!");
+        } else {
+            System.out.println("This number is negative!");
+        }
+    }
+}
