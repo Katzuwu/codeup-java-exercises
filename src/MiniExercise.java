a819191a3c00684109b3567f88c443feeb8049cb
Ian Arellano <ian.g.arellano01@gmail.com>
2021-09-07 10:29:34 -0500

    add new exercise to miniexercise

--- src/MiniExercise.java
+++ src/MiniExercise.java
@@ -3,17 +3,26 @@ import java.util.Scanner;
 public class MiniExercise {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int x = 5;
-        int y = -5;
-        if (x > 0) {
-            System.out.println("This number is positive!");
+//        int x = 5;
+//        int y = -5;
+//        if (x > 0) {
+//            System.out.println("This number is positive!");
+//        } else {
+//            System.out.println("This number is negative!");
+//        }
+//        if (y > 0) {
+//            System.out.println("This number is positive!");
+//        } else {
+//            System.out.println("This number is negative!");
+//        }
+        System.out.println("Hello User! How old are you?");
+        int userAge = scanner.nextInt();
+        if(userAge >= 20) {
+            System.out.println("You are older than a teenager!");
+        } else if (userAge >= 13) {
+            System.out.println("Wow! You are a teenager!");
         } else {
-            System.out.println("This number is negative!");
-        }
-        if (y > 0) {
-            System.out.println("This number is positive!");
-        } else {
-            System.out.println("This number is negative!");
+            System.out.println("Sorry, you are younger than a teenager :(");
         }
     }
 }
