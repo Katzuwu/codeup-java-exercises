b276a3b509eede2cc60757ea8c5ae52ab25f195e
Ian Arellano <ian.g.arellano01@gmail.com>
2021-09-09 11:58:26 -0500

    add MethodWarmup

--- src/MethodWarmup.java
+++ src/MethodWarmup.java
@@ -1,7 +1,8 @@
 public class MethodWarmup {
     public static void main(String[] args) {
-        System.out.println(getDifference(14, 24));
-        System.out.println(getDifference(14));
+//        System.out.println(getDifference(14, 24));
+//        System.out.println(getDifference(14));
+        System.out.println(addNums(5));
     }
     public static int getDifference(int one, int two){
         if(two > one){
@@ -13,4 +14,11 @@ public class MethodWarmup {
     public static int getDifference(int one){
         return one - 0;
     }
+    // TODO: use recursion to add all numbers up from 1 to a given number
+     public static int addNums(int num) {
+         if (num==1)
+             return 1;
+         else
+             return addNums(num-1)+num;
+     }
 }
