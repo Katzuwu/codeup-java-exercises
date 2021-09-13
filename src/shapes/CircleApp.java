69036edd7cb40f0ed58463ec51caff722385a40c
Ian Arellano <ian.g.arellano01@gmail.com>
2021-09-13 09:41:37 -0500

    refactored CircleApp.java to be correct. Used Scanner instead of Input like the problem wanted. Refactored using Input

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,13 +1,13 @@
 package shapes;
 
 import util.Input;
-import java.util.Scanner;
+import util.Input;
 
 public class CircleApp {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Give me any number, even one with a decimal, and that will be the number of the radius of your circle.");
-        double userRadius = scanner.nextDouble();
+        Input input = new Input();
+        double userRadius = input.getDouble();
+        System.out.println("Okay, we're going to make a circle out of that number");
         Circle userCircle = new Circle(userRadius);
         System.out.println("Okay! Here is your circumference: " + userCircle.getCircumference());
         System.out.println("And here is your area: " + userCircle.getArea());
