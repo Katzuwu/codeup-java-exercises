2b55dba1a5cc8ea40370bf49df3e7b5269a13c2c
Ian Arellano <ian.g.arellano01@gmail.com>
2021-10-14 09:27:49 -0500

    I don't remember what TestTest was. But add JavaDrills

---
+++ src/JavaDrills.java
@@ -0,0 +1,18 @@
+import java.lang.StringBuilder;
+
+//almost done. just need to import swapCase;
+public class JavaDrills {
+	public static String flipOuterCase(String str){
+		String first = str.substring(0, 1).swapCase;
+		String last = str.substring((str.length() - 1)).toUpperCase();
+		String middle = str.substring(1, str.length() - 1);
+		return first + middle + last;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(flipOuterCase("cat")); // CaT
+		System.out.println(flipOuterCase("CaT")); // cat
+		System.out.println(flipOuterCase("caT")); // Cat
+		System.out.println(flipOuterCase("cAt")); // CAT
+	}
+}
